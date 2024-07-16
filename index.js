fd896412ef4129aeed46109eb8d86848b1ec45a6
Gokuktkn <120592109+Gokuktkn@users.noreply.github.com>
2024-07-16 15:45:35 +0700

    Create project

---
+++ index.js
@@ -0,0 +1,22 @@
+import express from 'express'
+import cors from 'cors'
+import { config } from 'dotenv'
+
+const app = express();
+
+config();
+
+app.use(express.json());
+app.use(cors());
+
+app.use((err, req, res, next) => {
+    if (err.message) {
+        return res.json({ error: err.message });
+    } else {
+        return res.json({ err });
+    }
+});
+
+app.listen(process.env.PORT, (err) => {
+    console.log(`Your app is listening on ${process.env.PORT}`)
+})
