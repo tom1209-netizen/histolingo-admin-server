8c758ec25fb91ef9126ed664ec73e3d418d7b4a4
Gokuktkn <120592109+Gokuktkn@users.noreply.github.com>
2024-07-20 20:22:42 +0700

    Fix format

--- models/role.model.js
+++ models/role.model.js
@@ -1,11 +1,29 @@
 import mongoose, { Schema } from "mongoose";
+import { adminStatus } from "../constants/admin.constant.js";
 
 const roleSchema = new Schema({
-    name: { type: String, required: true },
-    status: { type: Number, enum: [0, 1], required: true, default: 1 },
-    permissions: { type: [String], required: true },
-    createdAt: { type: Date, default: Date.now },
-    updatedAt: { type: Date, default: Date.now }
+    name: {
+        type: String,
+        required: true
+    },
+    status: {
+        type: Number,
+        enum: [adminStatus.active, adminStatus.inactive],
+        required: true,
+        default: 1
+    },
+    permissions: {
+        type: [String],
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    updatedAt: {
+        type: Date,
+        default: Date.now
+    }
 });
 
 // Update when document save
--- models/token.model.js
+++ models/token.model.js
@@ -1,8 +1,16 @@
 import mongoose from "mongoose";
 
 const tokenSchema = new mongoose.Schema({
-    owner: { type: mongoose.Schema.Types.ObjectId, ref: "admin", require: true, unique: true },
-    refreshToken: { type: String, unique: true }
+    owner: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "admin",
+        require: true,
+        unique: true
+    },
+    refreshToken: {
+        type: String,
+        unique: true
+    }
 });
 
 export const tokenModel = mongoose.model("token", tokenSchema)
