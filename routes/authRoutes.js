9ee70b506511f99750e7f38f5ae8f6c1d4199e5f
tom1209-netizen <lephuckhang0@gmail.com>
2024-07-20 20:15:20 +0700

    Add email validator

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,7 +3,7 @@ import { forgotPassword, resetPassword } from '../controllers/authController.js'
 
 const router = Router();
 
-router.post('/forgot-password', forgotPassword);
+router.post('/forgot-password', validateEmail, forgotPassword);
 router.post('/reset-password', resetPassword);
 
 export default router;
