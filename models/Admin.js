006be88a08467af23074c87667642de97ba7b3b0
tom1209-netizen <lephuckhang0@gmail.com>
2024-07-20 12:02:41 +0700

    :qAdd admin model

---
+++ models/Admin.js
@@ -0,0 +1,46 @@
+import mongoose from "mongoose";
+const { Schema, model } = mongoose;
+
+const adminSchema = new Schema({
+  firstName: {
+        type: String,
+        required: true,
+        maxlength: 100,
+    },
+    lastName: {
+        type: String,
+        required: true,
+        maxlength: 100,
+    },
+    adminName: {
+        type: String,
+        required: true,
+        maxlength: 100,
+    },
+    password: {
+        type: String,
+        required: true,
+        maxlength: 250,
+    },
+    roles: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "Role",
+            required: true,
+        },
+    ],
+    status: {
+        type: Number,
+        enum: [0, 1],
+        required: true,
+    },
+    supervisorId: {
+        type: Schema.Types.ObjectId,
+        ref: "Admin",
+        required: true,
+    },
+});
+
+const Admin = model("Admin", adminSchema);
+
+export default Admin;
