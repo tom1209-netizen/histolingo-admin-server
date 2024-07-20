4f348565a43ae5bdf889be80a32138296984325f
tom1209-netizen <lephuckhang0@gmail.com>
2024-07-20 19:36:50 +0700

    Add email field to admin schema

--- models/Admin.js
+++ models/Admin.js
@@ -17,6 +17,10 @@ const adminSchema = new Schema({
         required: true,
         maxlength: 100,
     },
+    email: {
+        type: String,
+        required: true,
+    },
     password: {
         type: String,
         required: true,
