697d68be779d5c0ad6a161b126a05759eb18bd00
tom1209-netizen <lephuckhang0@gmail.com>
2024-07-30 18:48:49 +0700

    Fix test to accdomodate new middleware

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+export default {
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.jsx?$': 'babel-jest',
+  },
+  moduleFileExtensions: ['js', 'jsx', 'json', 'node'],
+  testMatch: ['**/__tests__/**/*.test.js'],
+};
