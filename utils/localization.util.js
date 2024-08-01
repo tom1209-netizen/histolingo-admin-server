2c62cb480172d5809ebc30fc93606bacfdbc1f35
tom1209-netizen <lephuckhang0@gmail.com>
2024-08-01 16:55:04 +0700

    Add logic to add localization

---
+++ utils/localization.util.js
@@ -0,0 +1,22 @@
+import { localeData } from "../localization.js";
+
+export const t = (language, path, values = {}) => {
+    let currentLocaleData = localeData[language];
+
+    const parts = path.split('.');
+    let target = currentLocaleData;
+    for (const key of parts) {
+        if (target[key]) {
+            target = target[key];
+        } else {
+            target = null;
+            break;
+        }
+    }
+    if (typeof target === 'string') {
+        for (const key in values) {
+            target = target.replace(new RegExp(`{{${key}}}`, 'g'), values[key]);
+        }
+    }
+    return target;
+};
