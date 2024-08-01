a97f3a7befe9f483617eedfb5e12886e88a200aa
Lee Khan <lephuckhang0@gmail.com>
2024-08-01 17:31:51 +0700

    Remove language field from the country model

--- models/country.model.js
+++ models/country.model.js
@@ -1,11 +1,18 @@
 import mongoose from "mongoose";
-import { languageField } from "../utils/language.utils.js";
 const { Schema, model } = mongoose;
 
 const countrySchema = new Schema(
     {
-        name: languageField(250),
-        description: languageField(1000),
+        name: {
+            type: String,
+            require: true,
+            maxLength: 250,
+        },
+        description: {
+            type: String,
+            require: true,
+            maxLength: 1000,
+        },
         image: {
             type: String,
             require: true,
